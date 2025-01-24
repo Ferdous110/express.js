d05a94f12d70a264eb4540c1a2f8432eb54bfdf8
ANJUM FERDOUS <anjumferdous110@gmail.com>
2025-01-24 11:22:57 +0600

    Methods and use postman

--- lesson-1/app.js
+++ lesson-1/app.js
@@ -2,12 +2,25 @@ const express = require('express');
 const app = express();
 
 
-
-
-
-
-
-
-
+app.get("/", (req, res) => {
+  res.send("i am a get request at home route ");
+  res.end();
+});
+app.get("/about", (req, res) => {
+  res.send("i am a get request at about route ");
+  res.end();
+});
+app.post("/", (req, res) => {
+  res.send("i am a post request at home route ");
+  res.end();
+});
+app.put("/", (req, res) => {
+  res.send("i am a put request at home route ");
+  res.end();
+});
+app.delete("/", (req, res) => {
+  res.send("i am a delete request at home route ");
+  res.end();
+});
 
 module.exports = app;
