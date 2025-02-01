97e8d32eac50b05eed3d9e4ce1aec08cfc0899f0
ANJUM FERDOUS <anjumferdous110@gmail.com>
2025-02-01 11:05:44 +0600

    user nad login validatoon

--- express-validator/index.js
+++ express-validator/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { body } = require("express-validator");
+const { validationResult } = require("express-validator")
 const app = express();
 
 const PORT = 5443;
@@ -16,10 +18,48 @@ app.get("/test", (req, res) => {
 
 // api/register
 
-app.post("/api/register", (req, res)=>{
+app.post("/api/register", 
+    //input validation
+    body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Name is missing")
+    .isLength({min: 5})
+    .withMessage("Name must have at least 5 characters")
+    .isLength({max: 31})
+    .withMessage("Name can have maximum 31 characters")
+    ,
+    body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("email is missing")
+    .isEmail()
+    .withMessage("not a valid email")
+    ,
+    body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("password is missing")
+    .isLength({min: 5})
+    .withMessage("password must have at least 5 characters")
+    ,
+    body("dob")
+    .trim()
+    .notEmpty()
+    .withMessage("dob is missing")
+    .isISO8601()
+    .toDate()
+    .withMessage("not a valid dob")
+    ,(req, res, next) => {
+        const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array()});
+  }
+  next();
+    },
+    (req, res)=>{
     try {
         const { name, email, password, dob } = req.body;
-        console.log(req.body)
         const newUser = {
             name, email, password, dob
         }
@@ -27,6 +67,49 @@ app.post("/api/register", (req, res)=>{
             message: "user was created",
             user: newUser,
         });
+    } catch (error) {
+        return res.json({
+            message: error.message
+        })
+    }
+})
+
+app.post("/api/login", 
+    //input validation
+    
+    body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("email is missing")
+    .isEmail()
+    .withMessage("not a valid email")
+    ,
+    body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("password is missing")
+    .isLength({min: 5})
+    .withMessage("password must have at least 5 characters")
+    ,(req, res, next) => {
+        const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array()});
+  }
+  next();
+    },
+    (req, res)=>{
+    try {
+        const {  email, password } = req.body;
+        if(email === "anjumferdous110@gmail.com" && password === "12345") {
+            return res.status(200).json({
+                message: "user was logged In", 
+            });
+        } else{
+            return res.status(404).json({
+                message: "email and password do not mess", 
+            });
+        }
+        
     } catch (error) {
         return res.json({
             message: error.message
